35ce31f67af046fa4ea27967c5184ec4fcacdfcc
Loghin Alexandru <loghinalexandru61@gmail.com>
2021-10-23 16:07:27 +0300

    Add UI initial install

---
+++ src/engine/engine.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+original = cv2.imread("../misc/test_4.jpg")
+hsv = cv2.cvtColor(original, cv2.COLOR_BGR2HSV)
+green_mask = cv2.inRange(hsv, (25, 52, 72), (102, 255,255))
+result = cv2.bitwise_and(original,original, mask=green_mask)
+result = cv2.morphologyEx(result, cv2.MORPH_DILATE, cv2.getStructuringElement(cv2.MORPH_RECT, (10,10)))
+gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+_, gray = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
+gray = cv2.morphologyEx(gray, cv2.MORPH_ERODE, cv2.getStructuringElement(cv2.MORPH_RECT, (5,5)))
+cv2.imwrite("result.jpg", cv2.bitwise_not(gray))
--- src/main.py
+++ src/main.py
@@ -1,12 +1,25 @@
-import cv2
-import numpy as np
+import kivy
+import glob
+import os
+kivy.require('2.0.0')
 
-original = cv2.imread("../misc/test_2.jpg")
-hsv = cv2.cvtColor(original, cv2.COLOR_BGR2HSV)
-green_mask = cv2.inRange(hsv, (25, 52, 72), (102, 255,255))
-result = cv2.bitwise_and(original,original, mask=green_mask)
-result = cv2.morphologyEx(result, cv2.MORPH_DILATE, cv2.getStructuringElement(cv2.MORPH_RECT, (10,10)))
-gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
-_, gray = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
-gray = cv2.morphologyEx(gray, cv2.MORPH_ERODE, cv2.getStructuringElement(cv2.MORPH_RECT, (5,5)))
-cv2.imwrite("result.jpg", cv2.bitwise_not(gray))
+from kivy.app import App
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.image import Image
+
+class ImageButton(ButtonBehavior, Image):
+    pass
+
+class BlackBoardGreenBoardApp(App):
+    kv_directory = 'modules'
+
+    def build(self):
+        app_folder = os.path.dirname(os.path.abspath(__file__))
+        files = os.listdir(app_folder)
+        for file in files:
+            print(file)
+            if(file.endswith("jpg")):
+                self.root.ids.gallery_content.add_widget(ImageButton(source=file))
+
+if __name__ == '__main__':
+    BlackBoardGreenBoardApp().run()
