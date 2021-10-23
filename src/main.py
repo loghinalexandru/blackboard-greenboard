3e7ea1603623a1ee68ef55c6032f9f556d6dd2a9
Loghin Alexandru <loghinalexandru61@gmail.com>
2021-10-23 12:46:24 +0300

    Add more test data & good inital processing

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
 import cv2
 import numpy as np
 
-original = cv2.imread("test.jpg")
+original = cv2.imread("../misc/test_2.jpg")
 hsv = cv2.cvtColor(original, cv2.COLOR_BGR2HSV)
-green_mask = cv2.inRange(hsv, (36, 0, 0), (100, 255,255))
+green_mask = cv2.inRange(hsv, (25, 52, 72), (102, 255,255))
 result = cv2.bitwise_and(original,original, mask=green_mask)
-kernel = np.ones((3, 3), np.uint8)
-result = cv2.morphologyEx(result, cv2.MORPH_ERODE, kernel)
+result = cv2.morphologyEx(result, cv2.MORPH_DILATE, cv2.getStructuringElement(cv2.MORPH_RECT, (10,10)))
 gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("gray.jpg", gray)
-result = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-cv2.imwrite("result.jpg", result)
+_, gray = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
+gray = cv2.morphologyEx(gray, cv2.MORPH_ERODE, cv2.getStructuringElement(cv2.MORPH_RECT, (5,5)))
+cv2.imwrite("result.jpg", cv2.bitwise_not(gray))
