03f81a65d541489c70d36125f602c140f5a887bc
Loghin Alexandru <loghinalexandru61@gmail.com>
2021-11-01 18:55:57 +0200

    Update capturescreen.py

--- src/libs/capturescreen.py
+++ src/libs/capturescreen.py
@@ -8,12 +8,6 @@ from engine.main import process_image_with_rotation
 from constants import Screen
 
 class CaptureScreen(MDScreen):
-    def on_pre_enter(self):
-        Clock.schedule_once(lambda _: self.ids.camera.force_landscape())
-
-    def on_leave(self):
-        Clock.schedule_once(lambda _: self.ids.camera.restore_orientation())
-
     def picture_taken(self, _, filename):
         Clock.schedule_once(partial(process_image_with_rotation, filename))
         Clock.schedule_once(partial(self.manager.get_screen(Screen.Gallery.value).add_photo, filename))
