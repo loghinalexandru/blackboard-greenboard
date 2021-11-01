bd3f596f1e3de2c6f4dfebc307a6eb6cd30747c8
Loghin Alexandru <loghinalexandru61@gmail.com>
2021-11-01 23:20:42 +0200

    Add camera length as window

--- src/libs/capturescreen.py
+++ src/libs/capturescreen.py
@@ -4,16 +4,16 @@ kivy.require('2.0.0')
 from functools import partial
 from kivymd.uix.screen import MDScreen
 from kivy.clock import Clock
+from kivy.core.window import Window
 from engine.main import process_image_with_rotation
 from constants import Screen
 
 class CaptureScreen(MDScreen):
-    def on_pre_enter(self):
-        Clock.schedule_once(lambda _: self.ids.camera.force_landscape())
+    def __init__(self, **kwargs):
+        super(CaptureScreen, self).__init__(**kwargs)
+        self.camera_resolution = Window.size
 
-    def on_pre_leave(self):
-        Clock.schedule_once(lambda _: self.ids.camera.restore_orientation())
-
-    def picture_taken(self, _, filename):
-        Clock.schedule_once(partial(process_image_with_rotation, filename))
-        Clock.schedule_once(partial(self.manager.get_screen(Screen.Gallery.value).add_photo, filename))
+    def on_picture_taken(self):
+        return True
+        # Clock.schedule_once(partial(process_image_with_rotation, filename))
+        # Clock.schedule_once(partial(self.manager.get_screen(Screen.Gallery.value).add_photo, filename))
