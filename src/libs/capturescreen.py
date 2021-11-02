64b049ea4b34100db866d4684787a5014d682e05
Loghin Alexandru <loghinalexandru61@gmail.com>
2021-11-02 21:28:32 +0200

    Add working version for the camera

--- src/libs/capturescreen.py
+++ src/libs/capturescreen.py
@@ -4,14 +4,41 @@ kivy.require('2.0.0')
 from functools import partial
 from kivymd.uix.screen import MDScreen
 from kivy.clock import Clock
-from kivy.core.window import Window
 from engine.main import process_image_with_rotation
-from constants import Screen
+from kivy.graphics.texture import Texture
+from kivy.uix.camera import Camera
+import numpy as np
+import cv2
+
+class AndroidCamera(Camera):
+    camera_resolution = (1920, 1080)
+
+    def _camera_loaded(self, *largs):
+        self.texture = Texture.create(size=np.flip(self.camera_resolution), colorfmt='rgb')
+        self.texture_size = list(self.texture.size)
+
+    def on_tex(self, *l):
+        if self._camera._buffer is None:
+            return None
+        frame = self.frame_from_buf()
+        self.frame_to_screen(frame)
+        super(AndroidCamera, self).on_tex(*l)
+
+    def frame_from_buf(self):
+        w, h = self.resolution
+        frame = np.frombuffer(self._camera._buffer.tostring(), 'uint8').reshape((h + h // 2, w))
+        frame_bgr = cv2.cvtColor(frame, 93)
+        return np.rot90(frame_bgr, 3)
+
+    def frame_to_screen(self, frame):
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        flipped = np.flip(frame_rgb, 0)
+        buf = flipped.tostring()
+        self.texture.blit_buffer(buf, colorfmt='rgb', bufferfmt='ubyte')
 
 class CaptureScreen(MDScreen):
     def __init__(self, **kwargs):
         super(CaptureScreen, self).__init__(**kwargs)
-        self.camera_resolution = (1920,1080)
 
     def on_picture_taken(self):
         return True
